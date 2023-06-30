eab1ebd42ed27ed071eecab0c19ca18a85de4ac5
LubomirP <lubomirpacheliev@gmail.com>
2023-06-30 17:04:04 +0300

    coupling the frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-def get_root():
-    return {"Hello, ": "FastAPI!"}
+@app.get("/api")
+async def get_root():
+    return "Welcome to the FastAPI server!"
+
+app.mount("/", StaticFiles(directory="../frontend/dist", html=True), name="dist")
